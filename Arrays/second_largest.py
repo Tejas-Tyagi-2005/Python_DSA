b4c619c868448c5e0b93e164f0ed3fbe7c777500
Tejas Tyagi <tejas@mywork.com>
2026-03-27 22:06:02 +0530

    # Return the second largest element in the list. # Assume all elements are distinct.

---
+++ Arrays/second_largest.py
@@ -0,0 +1,19 @@
+# Return the second largest element in the list.
+# Assume all elements are distinct.
+
+def sec_lar(arr):
+    max_val = arr[0]
+
+    sec_val = float('inf')
+
+    for i in arr:
+        if i > max_val:
+            sec_val = max_val
+            max_val = i 
+        if i > sec_val and i < max_val:    
+            sec_val = i
+    return sec_val
+
+print(sec_lar([1,2,3,4,5,9]))        
+
+
