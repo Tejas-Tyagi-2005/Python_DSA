fa8a3a27c4cac3757ecdc47dd16efe43c7c4248f
Tejas Tyagi <tejas@mywork.com>
2026-04-04 16:50:17 +0530

    # Find the character with the highest frequency in a string. # Return the character.

---
+++ Final_Arrays/q6_most_frequent_char.py
@@ -0,0 +1,32 @@
+# Find the character with the highest frequency in a string.
+# Return the character.
+
+def highest_freq(s):
+
+    max_char = ''
+    max_count = 0 
+
+    freq = {}
+
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else :
+            freq[char] = 1
+
+
+    for char in freq:
+        if freq[char] > max_count:
+            max_count = freq[char]
+            max_char = char 
+
+    return max_char
+
+
+
+
+
+
+                
+
+    
