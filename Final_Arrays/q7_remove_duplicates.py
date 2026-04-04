a0e2542b7b179d70b7cc8c81e91aae2af8022dba
Tejas Tyagi <tejas@mywork.com>
2026-04-04 17:05:21 +0530

    commented version for better understanding

--- Final_Arrays/q7_remove_duplicates.py
+++ Final_Arrays/q7_remove_duplicates.py
@@ -3,13 +3,13 @@
 
 def duplicate_remover(s):
     s = s.lower()
-    seen = set()
-    result = ""
+    seen = set() # collection of elements already seen
+    result = "" # building the new string ,only adding unique elements
 
     for char in s:
-        if char not in seen:
+        if char not in seen: # if char is not in seen set 
             result += char 
-            seen.add(char)
+            seen.add(char) # add it in seen set  , so it is not added again 
 
     return result 
 
