a455a63d12afe3306aef6f0822775bd1c89e194f
Tejas Tyagi <tejas@mywork.com>
2026-08-12 18:06:01 +0530

    Duplicate numbers edge case solved

--- AAE_BANK/Large Small Sum.py
+++ AAE_BANK/Large Small Sum.py
@@ -3,34 +3,34 @@
 # The sum of the second-largest element and the smallest element.
 
 
-def helper(arr):
+def largeSmall(arr):
 
     largest = arr[0]
 
-    second_largest = 0
-
-    for i in range(len(arr)):
-        if arr[i] > largest and arr[i] > second_largest:
-            second_largest = largest
-            largest = arr[i]
-    return second_largest
-
-def largeSmall(arr):
-
-    second_largest  = helper(arr)
 
     smallest = arr[0]
 
-    for i in arr:
-        if i < smallest:
-            smallest = i
-
-    return smallest + second_largest
+    second_largest = None 
 
 
+    for i in range(1,len(arr)):
+        if arr[i] > largest:
+            second_largest = largest
+            largest = arr[i]
+
+        
+        elif arr[i] != largest and (second_largest is None or  arr[i] > second_largest):
+            second_largest = arr[i]
+
+
+        if arr[i] < smallest:
+            smallest = arr[i]
+
+        
+    val = smallest + second_largest
+    return val 
 
 
 
 
-
-
+                     
